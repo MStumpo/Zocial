2b0ff3ac70b94c0e801ef37e43c529da0c23145b
MStumpo <stumpo.miguel@gmail.com>
2018-12-26 23:05:52 +0000

    Attempt at adding username feature

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+users = [];
+connections = [];
+
+
+http.listen(port, function(){
+  console.log('listening on port ' + port + ', time to... try not to fail...');
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
@@ -20,23 +27,32 @@ app.get('/about', function(req, res){
 //CHAT SYSTEM 
 
 io.on('connection', function(socket){
-	 console.log('A new weirdo just joined');
-  // socket.username = "Completely average person";
-   //socket.on('change_username', function(data){
-    //socket.username = data.username;
-  //});
-  socket.on('disconnect', function(){
-    console.log('Aaaand someone left :(');
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+  socket.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Connected: %s sockets connected', connections.length);
   });
 
+  socket.on('message', function(msg){ 
+    io.sockets.emit('message', msg);
+  });
+
+  socket.on('new user', function(data, callback){
+    console.log("I EXIST YOU CUNT");
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+  function updateUsernames(){
+    if(socket.usernames != null || []){
+    io.sockets.emit('get users', socket.usernames);
+    }else{
+      console.log('nobody here');
+    }
+  }
 });
 
-http.listen(port, function(){
-  console.log('listening on port ' + port + ', time to... try not to fail...');
-});
