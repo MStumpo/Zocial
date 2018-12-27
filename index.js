a3a77b6722c5be56a48e8b32d11ae402529a0d90
MStumpo <stumpo.miguel@gmail.com>
2018-12-27 22:23:26 +0000

    Fixing username feature + major tweaks

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+const url = require('url');
 users = [];
 connections = [];
-
 
 http.listen(port, function(){
   console.log('listening on port ' + port + ', time to... try not to fail...');
@@ -28,31 +28,29 @@ app.get('/about', function(req, res){
 
 io.on('connection', function(socket){
   connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
+  console.log('Connected: %s people connected', connections.length);
   socket.on('disconnect', function(data){
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
-    console.log('Connected: %s sockets connected', connections.length);
+    console.log('An arse left');
+    console.log('Connected: %s people connected', connections.length);
   });
 
-  socket.on('message', function(msg){ 
-    io.sockets.emit('message', msg);
+  socket.on('message', function(data){ 
+    io.sockets.emit('message', {msg : data, user: socket.username});
+    console.log(socket.username + ' --> ' + data);
   });
 
   socket.on('new user', function(data, callback){
-    console.log("I EXIST YOU CUNT");
-    callback(true);
+    if(callback) callback(true);
     socket.username = data;
     users.push(socket.username);
-    updateUsernames();
+    io.sockets.emit('get users', {users: data});
+    console.log("One weirdo named themselves "+ socket.username);
   });
   function updateUsernames(){
-    if(socket.usernames != null || []){
-    io.sockets.emit('get users', socket.usernames);
-    }else{
-      console.log('nobody here');
-    }
+        io.sockets.emit('get users', users);
   }
 });
 
